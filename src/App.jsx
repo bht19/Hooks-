864815ac1fee50b4e2f61cdb66db112aee4d73a5
bht19 <bharatbht52@gmail.com>
2025-01-20 19:59:16 +0000

    Updated code of Todos

--- src/App.jsx
+++ src/App.jsx
@@ -77,23 +77,131 @@
 
 // export default App
 
+// Todo custom Hook simple hook
+
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+// function useTodos(){
+//   const [todos, setTodos] = useState([]
+//     useEffect(() => {
+//       axios.get("https://sum-server.100xdevs.com/todos")
+//         .then(res => {
+//           setTodos(res.data.todos);
+//         })
+//     }, [])
+
+//     return todos;
+//   )
+// }
+// function App() {
+//   const todos = useTodos();
+
+//   return (
+//     <>
+//       {todos.map(todo => <Track todo={todo} />)}
+//     </>
+//   )
+// }
+
+// function Track({ todo }) {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
+
+// export default App
+
+
+// including a loading parameter
+
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+// function useTodos() {
+//   const [loading, setLoading] = useState(true);
+//   const [todos, setTodos] = useState([])
+
+//   useEffect(() => {
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//       .then(res => {
+//         setTodos(res.data.todos);
+//         setLoading(false);
+//       })
+//   }, [])
+
+//   return {
+//     todos: todos,
+//     loading: loading
+//   };
+// }
+
+// function App() {
+//   const { todos, loading } = useTodos();
+
+//   if (loading) {
+//     return <div>
+//       Loading...
+//     </div>
+//   }
+
+//   return (
+//     <>
+//       {todos.map(todo => <Track todo={todo} />)}
+//     </>
+//   )
+// }
+
+// function Track({ todo }) {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
+
+// export default App
+
+// Including a contineous fetch from the server
+
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-function useTodos(){
-  const [todos, setTodos] = useState([]
-    useEffect(() => {
+function useTodos(n) {
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const value = setInterval(() => {
       axios.get("https://sum-server.100xdevs.com/todos")
         .then(res => {
           setTodos(res.data.todos);
+          setLoading(false);
         })
-    }, [])
+    }, n * 1000)
+  
+    axios.get("https://sum-server.100xdevs.com/todos")
+      .then(res => {
+        setTodos(res.data.todos);
+        setLoading(false);
+      })
 
-    return todos;
-  )
+    return () => {
+      clearInterval(value)
+    }
+  }, [n])
+
+  return {todos, loading};
 }
+
 function App() {
-  const todos = useTodos();
+  const {todos, loading} = useTodos(10);
+
+  if (loading) {
+    return <div> loading... </div>
+  }
 
   return (
     <>
