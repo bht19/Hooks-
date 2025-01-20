4bad25945a694a1cfdefe78fae6f0f36c6644e2e
bht19 <bharatbht52@gmail.com>
2025-01-20 19:29:44 +0000

    Updated feature X or fixed bug Y

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,113 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useEffect, useState } from 'react'
+// import './App.css'
+// import React from 'react';
 
+// function App() {
+ 
+//   const [render, setRender] = useState(true);
+
+//   useEffect(() =>{
+//     setInterval(() =>{ 
+//       setRender(r => !r);
+//     },5000)
+//   }, []);
+
+//   return (
+//     <>
+//       {render ? <MyComponent /> : <div> Empty </div>}
+//     </>
+//   )
+// }
+
+// Functional Events
+
+// function MyComponent() {
+//   const [count, setCount] = useState(0);
+
+//   const incrementCount = () => {
+//     setCount(count +1);
+//   };
+
+//   return (
+//     <div>
+//       <p> {count} </p>
+//       <button onClick={incrementCount}>Increment</button>
+//     </div>
+//   );
+// }
+
+// Classical Events
+
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//   }
+
+//   incrementCount = () => {
+//     this.setState({ count: this.state.count + 1 });
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <p>{this.state.count}</p>
+//         <button onClick={this.incrementCount}>Increment</button>
+//       </div>
+//     );
+//   }
+
+// LifeCycle Events
+
+// function MyComponent () {
+//   useEffect(() => {
+//     console.error("component Mounted");
+
+//     return () => {
+//       console.log("Component Unmounted");
+//     };
+//   }, []);
+
+//   return <div>
+//     From inside My Component
+//   </div>
+// }
+
+
+
+// export default App
+
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+function useTodos(){
+  const [todos, setTodos] = useState([]
+    useEffect(() => {
+      axios.get("https://sum-server.100xdevs.com/todos")
+        .then(res => {
+          setTodos(res.data.todos);
+        })
+    }, [])
+
+    return todos;
+  )
+}
 function App() {
-  const [count, setCount] = useState(0)
+  const todos = useTodos();
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {todos.map(todo => <Track todo={todo} />)}
     </>
   )
+}
+
+function Track({ todo }) {
+  return <div>
+    {todo.title}
+    <br />
+    {todo.description}
+  </div>
 }
 
 export default App
