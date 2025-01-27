2a5432d45cfdea50c2526b714e846213ebddc127
bht19 <bharatbht52@gmail.com>
2025-01-27 16:11:10 +0000

    Updated code of Todos

--- src/App.jsx
+++ src/App.jsx
@@ -164,58 +164,60 @@
 
 // export default App
 
-// Including a contineous fetch from the server
+// // Including a contineous fetch from the server with Loading displayed 
 
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+// import { useEffect, useState } from 'react'
+// import axios from 'axios'
 
-function useTodos(n) {
-  const [todos, setTodos] = useState([])
-  const [loading, setLoading] = useState(true);
+// function useTodos(n) {
+//   const [todos, setTodos] = useState([])
+//   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const value = setInterval(() => {
-      axios.get("https://sum-server.100xdevs.com/todos")
-        .then(res => {
-          setTodos(res.data.todos);
-          setLoading(false);
-        })
-    }, n * 1000)
+//   useEffect(() => {
+//     const value = setInterval(() => {
+//       axios.get("https://sum-server.100xdevs.com/todos")
+//         .then(res => {
+//           setTodos(res.data.todos);
+//           setLoading(false);
+//         })
+//     }, n * 1000)
   
-    axios.get("https://sum-server.100xdevs.com/todos")
-      .then(res => {
-        setTodos(res.data.todos);
-        setLoading(false);
-      })
+//     axios.get("https://sum-server.100xdevs.com/todos")
+//       .then(res => {
+//         setTodos(res.data.todos);
+//         setLoading(false);
+//       })
 
-    return () => {
-      clearInterval(value)
-    }
-  }, [n])
+//     return () => {
+//       clearInterval(value)
+//     }
+//   }, [n])
 
-  return {todos, loading};
-}
+//   return {todos, loading};
+// }
 
-function App() {
-  const {todos, loading} = useTodos(10);
+// function App() {
+//   const {todos, loading} = useTodos(10);
 
-  if (loading) {
-    return <div> loading... </div>
-  }
+//   if (loading) {
+//     return <div> loading... </div>
+//   }
 
-  return (
-    <>
-      {todos.map(todo => <Track todo={todo} />)}
-    </>
-  )
-}
+//   return (
+//     <>
+//       {todos.map(todo => <Track todo={todo} />)}
+//     </>
+//   )
+// }
 
-function Track({ todo }) {
-  return <div>
-    {todo.title}
-    <br />
-    {todo.description}
-  </div>
-}
+// function Track({ todo }) {
+//   return <div>
+//     {todo.title}
+//     <br />
+//     {todo.description}
+//   </div>
+// }
 
-export default App
+// export default App
+
+
